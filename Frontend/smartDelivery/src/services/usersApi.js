c22a339da497a0ea2497794764fb5cc5215ce53e
Dakshata <149934864+Dakshata-Takarkhede@users.noreply.github.com>
2024-12-13 12:45:06 +0530

    Update usersApi.js

--- Frontend/smartDelivery/src/services/usersApi.js
+++ Frontend/smartDelivery/src/services/usersApi.js
@@ -1,32 +1,32 @@
 import api from "./api.js";
 
 // Register a new user with avatar upload
-export const registerUser = (formData) => api.post("/users/register", formData, {
+export const registerUser = (formData) => api.post("/api/v1/users/register", formData, {
     headers: { "Content-Type": "multipart/form-data" },
 });
 
 // Log in a user
-export const loginUser = (credentials) => api.post("/users/login", credentials);
+export const loginUser = (credentials) => api.post("/api/v1/users/login", credentials);
 
 // Log out the current user
-export const logoutUser = () => api.post("/users/logout");
+export const logoutUser = () => api.post("/api/v1/users/logout");
 
 // Refresh the access token
-export const refreshAccessToken = () => api.post("/users/refresh-token");
+export const refreshAccessToken = () => api.post("/api/v1/users/refresh-token");
 
 // Change the current user's password
-export const changePassword = (passwordData) => api.post("/users/change-password", passwordData);
+export const changePassword = (passwordData) => api.post("/api/v1/users/change-password", passwordData);
 
 // Get the details of the current user
-export const getCurrentUser = () => api.get("/users/current-user");
+export const getCurrentUser = () => api.get("/api/v1/users/current-user");
 
 // Update account details of the current user
-export const updateAccountDetails = (accountData) => api.patch("/users/update-account", accountData);
+export const updateAccountDetails = (accountData) => api.patch("/api/v1/users/update-account", accountData);
 
 // Update the avatar of the current user
-export const updateUserAvatar = (formData) => api.patch("/users/avatar", formData, {
+export const updateUserAvatar = (formData) => api.patch("/api/v1/users/avatar", formData, {
     headers: { "Content-Type": "multipart/form-data" },
 });
 
 // Get all users
-export const getAllUsers = () => api.get("/users");
+export const getAllUsers = () => api.get("/api/v1/users");
