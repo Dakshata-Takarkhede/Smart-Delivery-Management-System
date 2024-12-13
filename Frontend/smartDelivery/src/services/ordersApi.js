e6b54ecb1137fe9aeafa111747db6927b83bb2d6
Dakshata <149934864+Dakshata-Takarkhede@users.noreply.github.com>
2024-12-13 12:45:32 +0530

    Update ordersApi.js

--- Frontend/smartDelivery/src/services/ordersApi.js
+++ Frontend/smartDelivery/src/services/ordersApi.js
@@ -1,13 +1,13 @@
 import api from "./api.js";
 
 // Fetch all orders
-export const fetchOrders = () => api.get("/orders/");
+export const fetchOrders = () => api.get("/api/v1/orders/");
 
 // Create a new order
-export const createOrder = (orderData) => api.post("/orders/assign", orderData);
+export const createOrder = (orderData) => api.post("/api/v1/orders/assign", orderData);
 
 // Update an order
-// export const updateOrder = (orderId, updatedData) => api.put(`/orders/${orderId}`, updatedData);
+// export const updateOrder = (orderId, updatedData) => api.put(`/api/v1/orders/${orderId}`, updatedData);
 
 // Delete an order
-// export const deleteOrder = (orderId) => api.delete(`/orders/${orderId}`);
+// export const deleteOrder = (orderId) => api.delete(`/api/v1/orders/${orderId}`);
