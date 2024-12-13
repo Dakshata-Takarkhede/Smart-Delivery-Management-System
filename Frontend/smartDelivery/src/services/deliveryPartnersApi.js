6eab83b86f7a51f5c01b4306d2739f87cdf0c5f7
Dakshata <149934864+Dakshata-Takarkhede@users.noreply.github.com>
2024-12-13 12:45:55 +0530

    Update deliveryPartnersApi.js

--- Frontend/smartDelivery/src/services/deliveryPartnersApi.js
+++ Frontend/smartDelivery/src/services/deliveryPartnersApi.js
@@ -1,13 +1,13 @@
 import api from "./api.js";
 
 // Fetch all partners
-export const fetchPartners = () => api.get("/partners/");
+export const fetchPartners = () => api.get("/api/v1/partners/");
 
 // Create a new partner
-export const createPartner = (partnerData) => api.post("/partners/", partnerData);
+export const createPartner = (partnerData) => api.post("/api/v1/partners/", partnerData);
 
 // Update a partner
-export const updatePartner = (partnerId, updatedData) => api.patch(`/partners/${partnerId}`, updatedData);
+export const updatePartner = (partnerId, updatedData) => api.patch(`/api/v1/partners/${partnerId}`, updatedData);
   
 // Delete a partner
-export const deletePartner = (partnerId) => api.delete(`/partners/${partnerId}`);
+export const deletePartner = (partnerId) => api.delete(`/api/v1/partners/${partnerId}`);
