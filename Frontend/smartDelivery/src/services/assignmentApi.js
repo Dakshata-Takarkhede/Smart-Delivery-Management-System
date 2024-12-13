1f2a20c957bc230b8b40fd0f06f64a8403ab8b76
Dakshata <149934864+Dakshata-Takarkhede@users.noreply.github.com>
2024-12-13 12:46:46 +0530

    Update assignmentApi.js

--- Frontend/smartDelivery/src/services/assignmentApi.js
+++ Frontend/smartDelivery/src/services/assignmentApi.js
@@ -1,14 +1,14 @@
 import api from "./api.js";
 
 // Assign an order to a partner
-export const assignOrder = (orderId, partnerName) => api.post(`/assignments/${orderId}`, { partnerName });
+export const assignOrder = (orderId, partnerName) => api.post(`/api/v1/assignments/${orderId}`, { partnerName });
 
 // Get assignment metrics
-export const fetchAssignmentMetrics = () => api.get("/assignments/assignment-metrics");
+export const fetchAssignmentMetrics = () => api.get("/api/v1/assignments/assignment-metrics");
 
 // Get assignment data
-export const fetchAssignments = () => api.get("assignments/");
+export const fetchAssignments = () => api.get("/api/v1/assignments/");
   
 // Mark an assignment as failed
-export const failAssignment = (assignmentId, reason) => api.patch(`/assignments/fail/${assignmentId}`, reason );
+export const failAssignment = (assignmentId, reason) => api.patch(`/api/v1/assignments/fail/${assignmentId}`, reason );
 
